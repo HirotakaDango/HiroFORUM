7980e4f7e9c221810c64336c115d02758ad9f81c
赤葦だんご <104591072+HirotakaDango@users.noreply.github.com>
2024-01-09 22:56:08 +0700

    Add files via upload

--- header.php
+++ header.php
@@ -1,10 +1,10 @@
     <div class="container">
       <div class="d-flex flex-column flex-md-row align-items-center">
-        <a href="index.php" class="d-flex align-items-center link-body-emphasis text-decoration-none">
-          <h1 class="fw-bold text-center mb-4 mt-5">HiroFORUM</h1>
+        <a href="index.php" class="link-body-emphasis text-decoration-none fw-bold fs-3 mt-4">
+          HiroFORUM
         </a>
 
-        <nav class="d-inline-flex mt-md-0 ms-md-auto gap-4">
+        <nav class="d-inline-flex mt-md-0 ms-md-auto mt-2 pt-3 gap-4">
           <a class="btn btn-outline-light border-0 fw-bold rounded <?php if(basename($_SERVER['PHP_SELF']) == 'index.php') echo 'active' ?>" href="index.php"><small>home</small></a>
           <a class="btn btn-outline-light border-0 fw-bold rounded <?php if(basename($_SERVER['PHP_SELF']) == 'upload.php') echo 'active' ?>" href="upload.php"><small>upload</small></a>
           <a class="btn btn-outline-light border-0 fw-bold rounded <?php if(basename($_SERVER['PHP_SELF']) == 'settings.php') echo 'active' ?>" href="settings.php"><small>settings</small></a>
